ccc84ea7a77ef3d958fd92a7b0b840ff8ea29b38
Jonas de Laporte <j.laporte@tv1.eu>
2026-02-04 10:17:00 +0100

    fix: speakers is an array

--- src/batch/models/transcription_config_speaker_diarization_config.rs
+++ src/batch/models/transcription_config_speaker_diarization_config.rs
@@ -34,7 +34,7 @@ pub struct TranscriptionConfigSpeakerDiarizationConfig {
 
     /// Use this option to provide speaker labels linked to their speaker identifiers. When passed, the transcription system will tag spoken words in the transcript with the provided speaker labels whenever any of the specified speakers is detected in the audio. A maximum of 50 speakers identifiers across all speakers can be provided.
     #[serde(rename = "speakers", skip_serializing_if = "Option::is_none")]
-    pub speakers: Option<Box<models::TranscriptionConfigSpeakerDiarizationConfigSpeakers>>,
+    pub speakers: Option<Vec<models::TranscriptionConfigSpeakerDiarizationConfigSpeakers>>,
 }
 
 impl TranscriptionConfigSpeakerDiarizationConfig {
